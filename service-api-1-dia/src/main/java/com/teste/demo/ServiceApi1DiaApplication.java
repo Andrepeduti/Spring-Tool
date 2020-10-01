046af9115e76242472771c3da2f27d1d5a103b45
unknown <andre.peduti@hotmail.com>
2020-10-01 19:13:12 -0300

    Criação de API

---
+++ service-api-1-dia/src/main/java/com/teste/demo/ServiceApi1DiaApplication.java
@@ -0,0 +1,13 @@
+package com.teste.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApi1DiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApi1DiaApplication.class, args);
+	}
+
+}
